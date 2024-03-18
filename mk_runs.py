48552e9d7cf72e254681b721f7b1d608ea2712c4
LMT Helpdesk <mwatstein@umass.edu>
2024-03-18 19:46:57 +0000

    modified:   comments.txt 	modified:   mk_runs.py

--- mk_runs.py
+++ mk_runs.py
@@ -11,10 +11,10 @@ project="2024-S1-UM-9"
 #        obsnums per source (make it negative if not added to the final combination)
 on = {}
 on["goods-n-130"] = \
- [ 111610, 111611, 111612, 111614, 111615, 111616, 111618, 111619,
-   111620, 111623, 111624, 111625, 111627, 111628, 111629, 111631,
+ [ -111610, 111611, 111612, 111614, 111615, -111616, 111618, 111619,
+   111620, 111623, 111624, 111625, -111627, 111628, 111629, 111631,
    111632, 111633,
-   112388, 112389, 112390,]
+   -112388, 112389, 112390,]
 
 on["goods-n-2592"] = \
  [ 112392, 112393, 112394, 112397, 112398, 112399, 112401, 112402,
@@ -24,7 +24,7 @@ on["goods-n-2592"] = \
 #        common parameters per source on the first dryrun (run1a, run2a)
 pars1 = {}
 
-pars1["goods-n-130"] = ""
+pars1["goods-n-130"] = "speczoom=89,4 badcb=0/5"
 pars1["goods-n-2592"] = ""
 
 #        common parameters per source on subsequent runs (run1b, run2b)
